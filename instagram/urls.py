92513dcc2934a3d08cc6fd7645bfdc8b65a55511
arta <arta1382@gmail.com>
2023-01-04 21:05:32 +0330

    change views type

--- instagram/urls.py
+++ instagram/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('clone_data/', views.clone_data),
+    path('clone_data/', views.CloneData.as_view()),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,4 +1,4 @@
-from rest_framework import response, status, generics, permissions as perms, decorators
+from rest_framework import response, status, generics, permissions as perms, decorators, views
 from django.shortcuts import get_object_or_404
 from . import models, sample_data, serializers
 import requests, json, datetime
@@ -8,51 +8,53 @@ ACCESS_TOKEN = config('ACCESS_TOKEN')
 BASIC_URL = config('BASIC_URL')
 IG_USER_ID = config('IG_USER_ID')
 
-@decorators.api_view(['GET', ])
-@decorators.permission_classes([perms.AllowAny, ]) 
-def clone_data(request):
-    """
-    if you have access token in your .env file
-    dont send test param
-    else send ?test=1
-    """
-    page = request.GET.get('page')
-    duration = request.GET.get('duration', 30)
-    test = int(request.GET.get('test', 0))
 
-    if test:
-        dict_content = json.loads(sample_data.sample_res)
-    else:
-        use_url = BASIC_URL+IG_USER_ID+"?fields=business_discovery.username("+page+"){media{timestamp,comments_count,like_count,media_type}}&duration=&access_token="+ACCESS_TOKEN
-        req = requests.get(use_url)
-        dict_content = json.loads(req.content)
-        
+class CloneData(views.APIView):
+    permission_classes = [perms.AllowAny]
 
-    page_id = dict_content["business_discovery"]["ig_id"] 
-    need_medias = []
-    now = datetime.datetime.now()
-    for media in dict_content["business_discovery"]["media"]["data"]:
-        post_timestamp = datetime.datetime.fromisoformat(media["timestamp"]).replace(tzinfo=None)
-        timedelta =  now - post_timestamp
-        if timedelta.days <= duration :
-            values = {
-                "page_id":page_id,
-                "media_id":media["id"],
-                "created_at": post_timestamp,
-                "like_count":media["like_count"],
-                "comment_count":media["comments_count"],
-            }
-            if media["media_type"]=="CAROUSEL_ALBUM" : values.update({"media_type":0})
-            elif media["media_type"]=="IMAGE" : values.update({"media_type":1})
-            else : values.update({"media_type":2})
-            try:
-                post = models.Post.objects.create(**values)
-            except:
-                post = models.Post.objects.filter(media_id=values["media_id"]).update(**values)
-                post = models.Post.objects.filter(media_id=values["media_id"]).first()
+    def get(self, request, *args, **kwargs):
+        """
+        if you have access token in your .env file
+        dont send test param
+        else send ?test=1
+        """
+        page = request.GET.get('page')
+        duration = request.GET.get('duration', 30)
+        test = int(request.GET.get('test', 0))
 
-            need_medias.append(post)
+        if test:
+            dict_content = json.loads(sample_data.sample_res)
         else:
-            break
-    ser = serializers.PostSerilizer(need_medias, many=True)
-    return response.Response({"results":ser.data}, status=status.HTTP_200_OK)
+            use_url = BASIC_URL+IG_USER_ID+"?fields=business_discovery.username("+page+"){media{timestamp,comments_count,like_count,media_type}}&duration=&access_token="+ACCESS_TOKEN
+            req = requests.get(use_url)
+            dict_content = json.loads(req.content)
+            
+
+        page_id = dict_content["business_discovery"]["ig_id"] 
+        need_medias = []
+        now = datetime.datetime.now()
+        for media in dict_content["business_discovery"]["media"]["data"]:
+            post_timestamp = datetime.datetime.fromisoformat(media["timestamp"]).replace(tzinfo=None)
+            timedelta =  now - post_timestamp
+            if timedelta.days <= duration :
+                values = {
+                    "page_id":page_id,
+                    "media_id":media["id"],
+                    "created_at": post_timestamp,
+                    "like_count":media["like_count"],
+                    "comment_count":media["comments_count"],
+                }
+                if media["media_type"]=="CAROUSEL_ALBUM" : values.update({"media_type":0})
+                elif media["media_type"]=="IMAGE" : values.update({"media_type":1})
+                else : values.update({"media_type":2})
+                try:
+                    post = models.Post.objects.create(**values)
+                except:
+                    post = models.Post.objects.filter(media_id=values["media_id"]).update(**values)
+                    post = models.Post.objects.filter(media_id=values["media_id"]).first()
+
+                need_medias.append(post)
+            else:
+                break
+        ser = serializers.PostSerilizer(need_medias, many=True)
+        return response.Response({"results":ser.data}, status=status.HTTP_200_OK)
